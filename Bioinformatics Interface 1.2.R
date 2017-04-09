8dae9b65bd2296313543f9be1e96f1241f1f3d3e
derektoms <derek.toms@ucalgary.ca>
2017-04-09 14:18:52 -0600

    Update Bioinformatics Interface 1.2.R

--- Bioinformatics Interface 1.2.R
+++ Bioinformatics Interface 1.2.R
@@ -1,7 +1,37 @@
 library(shiny)
+# need a few more now...
+# if you need to install these to run on your local machine uncomment the following line:
+#install.packages(c('dplyr','tidyr','ggplot2','RColorBrewer','readr','stringr','shiny','shinythemes','shinyjs'))
+library(shinythemes)
+library(shinyjs)
 
-Selected <- mtcars
-Selected$category <- rep("Not assigned", nrow(Selected))
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(RColorBrewer)
+library(readr)
+library(stringr)
+
+# 2017-09-04
+# I have uploaded the CSV containing all series (GSE) using the human Affymetrix array (GPL570)
+# Our search will look through here for the appropriate keywords
+# 
+# In this version, I have just replaced the matrix (i.e. mtcards for all_gpl570)
+# Also made the checkboxes within the table (from 'http://stackoverflow.com/questions/26026740/rstudio-shiny-list-from-checking-rows-in-datatables')
+# Search function is not yet implemented in the UI
+#
+#
+all_gpl570<-read.csv('~/Dropbox/Retina_app/all_gpl570_april.csv')
+
+# search strings –– next version we take these from the UI
+search_string_motorNeurons = "\\bbrain\\b|\\bneurons?(al)?\\b&\\bmotor\\b"
+search_string_diabetes = "\\bpancreas\\b|\\bislets?\\b|\\bbeta-cells\\b|\\bdiabetes\\b"
+search_string_hepatocytes = "\\bliver\\b&\\bhepatocytes?\\b"
+search_string_cardiomyocytes = "\\bheart\\b&\\bcardiomyocytes?\\b"
+
+selected <- all_gpl570[,c(2,3,8)]
+selected$id <- 1:nrows(selected)
+selected$category <- rep("Not assigned", nrow(selected))
 
 ui <- fluidPage(
   #creation of a navigation bar and mulitple pages
@@ -22,7 +52,7 @@ ui <- fluidPage(
              tabPanel("Third Page", uiOutput("page3"), uiOutput("page3.1")
                       ),
              tabPanel("Fourth Page", uiOutput("page4"), 
-                      tableOutput("SelectedRows")
+                      dataTableOutput("SelectedRows")
                       )
   )
 )
@@ -32,6 +62,10 @@ server <- function(input, output) {
   output$cat1 <- renderText(input$cat1)
   output$cat2 <- renderText(input$cat2)
   output$cat3 <- renderText(input$cat3)
+  
+  # checkbox function
+  rowSelect <- reactive({paste(sort(unique(input[["rows"]])),sep=',')
+                         })
   output$page3 <- renderUI(
     fluidRow(
       column(3,
@@ -43,17 +77,26 @@ server <- function(input, output) {
       )
     )
   )
+  
   output$page3.1 <- renderUI(
-    fluidRow(
-      column(9,
-            renderTable(mtcars[seq(1, input$Key, 1),])
-      ),
-      column(3,
-            checkboxGroupInput("checkgroup", "Checked options are included", choices = seq(1, input$Key, 1), seq(3,input$Key, 3))
-      )
-    )
+    fluidRow(renderDataTable({
+      addCheckboxButtons<- paste0('<input type="checkbox" name="row', selected$id, '" value="', selected$id, '">',"")
+      cbind(Pick=addCheckboxButtons, selected[, , drop=FALSE])
+    }#)
+    #))}         
+    , options = list(orderClasses = TRUE, lengthMenu = c(5, 25, 50), pageLength = 25, escape=FALSE)
+    , callback = "function(table) {
+    table.on('change.dt', 'tr td input:checkbox', function() {
+      setTimeout(function () {
+         Shiny.onInputChange('rows', $(this).add('tr td input:checkbox:checked').parent().siblings(':last-child').map(function() {
+                 return $(this).text();
+              }).get())
+         }, 10); 
+    });
+}")
+  
   )
-  output$SelectedRows <- renderTable(mtcars[as.numeric(input$checkgroup),])
+  )
 }
 
 shinyApp(ui, server)
