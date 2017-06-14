0f7c1f297a5cc400cda5bb37e1f14b47fab4d5f4
VictorTong7 <victor7.tong@gmail.com>
2017-06-14 16:37:12 -0600

    Add files via upload

---
+++ Bioinformatics Interface 3.1.R
@@ -0,0 +1,109 @@
+library(shiny)
+# need a few more now...
+# if you need to install these to run on your local machine uncomment the following line:
+#install.packages(c('dplyr','tidyr','ggplot2','RColorBrewer','readr','stringr','shiny','shinythemes','shinyjs','DT'))
+library(shinythemes)
+library(shinyjs)
+library(DT)
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(RColorBrewer)
+library(readr)
+library(stringr)
+
+# 2017-09-04
+# I have uploaded the CSV containing all series (GSE) using the human Affymetrix array (GPL570)
+# Our search will look through here for the appropriate keywords
+# 
+# In this version, I have just replaced the matrix (i.e. mtcards for all_gpl570)
+# Also made the checkboxes within the table (from 'http://stackoverflow.com/questions/26026740/rstudio-shiny-list-from-checking-rows-in-datatables')
+# Search function is not yet implemented in the UI
+#
+#
+all_gpl570<-read.csv('all_gpl570.csv')
+
+# search strings -- next version we take these from the UI
+search_string_motorNeurons = "\\bbrain\\b|\\bneurons?(al)?\\b&\\bmotor\\b"
+search_string_diabetes = "\\bpancreas\\b|\\bislets?\\b|\\bbeta-cells\\b|\\bdiabetes\\b"
+search_string_hepatocytes = "\\bliver\\b&\\bhepatocytes?\\b"
+search_string_cardiomyocytes = "\\bheart\\b&\\bcardiomyocytes?\\b"
+
+# cut back the columns from the GSE info to make it more readable
+selected <- all_gpl570[,c(2,3,8)]
+selected2 <- selected
+
+# fill each row with 'not assigned' label, until they are placed in groups
+selected$category <- rep("Not assigned", nrow(selected))
+
+ui <- fluidPage(
+  #creation of a navigation bar and mulitple pages
+  navbarPage("Bioinformatics Software",
+             tabPanel("Search for GEO data series (GSE)",  
+                      #setting user inputed values and displaying values back at the user
+                      textInput("Key", "Enter search terms, separated by commas", value = ""),
+                      verbatimTextOutput("Key")
+             ),
+             tabPanel("Define categories for GEO samples (GSM)", uiOutput("page2"), 
+                      textInput("cat1", "Define Category 1"),
+                      verbatimTextOutput("cat1"),
+                      textInput("cat2", "Define Category 2"),
+                      verbatimTextOutput("cat2"),
+                      textInput("cat3", "Define Category 3"),
+                      verbatimTextOutput("cat3")
+             ),
+             # changed the format of this slightly to accomodate the DT package (for row selection)
+             tabPanel("Assign samples to categories", uiOutput("page3"), 
+                      DT::dataTableOutput("page3table"), 
+                      actionButton("Lock", "Assign Categories"), 
+                      verbatimTextOutput("Button"),
+                      verbatimTextOutput("Rows"),
+                      verbatimTextOutput("Currentcategory")
+                      
+             ),
+             # currently, page 4 displays the selected rows from page 3
+             tabPanel("Preview results", uiOutput("page4")
+                      
+             )
+  )
+)
+
+server <- function(input, output) {
+  output$Key <- renderText(unlist(strsplit(input$Key,",")))
+  output$cat1 <- renderText(input$cat1)
+  output$cat2 <- renderText(input$cat2)
+  output$cat3 <- renderText(input$cat3)
+  output$Button <- renderText(input$Lock)
+  output$Rows <- renderText(s())
+  output$Currentcategory <- renderText(currentcategory())
+  
+  
+  output$page3 <- renderUI(
+    fluidRow(
+      column(3,
+             selectInput("selection", "Select a Category",
+                         c("category1" <- {input$cat1},
+                           "category2" <- {input$cat2},
+                           "category3" <- {input$cat3},
+                           "category4" <- "Not included"))
+      )
+    )
+  )
+  
+  # render the table with the specified number of rows (will be keywords) without the option to further search (although we may want this)
+  output$page3table <- DT::renderDataTable(selected[1:8,], options=list(searching=FALSE))
+  
+  currentcategory <- eventReactive(input$Lock, input$selection)
+  
+  s <- eventReactive(input$Lock, input$page3table_rows_selected)
+  
+  selected2[s(),'category'] <- eventReactive(input$Lock, {currentcategory()})
+  
+  # display the whole table, but with the appropriate rows having the new category
+  # caveat here is that this refreshes every time
+  # next version needs to store this more permanently as an attribute of the table
+  # this will require a 'GO' button
+}
+
+shinyApp(ui, server)
